eb1670afac7c15137a3bf7a4c53b14e9fe69a8cc
karmugilan28 <btechaids23226@smvec.ac.in>
2025-01-23 07:07:54 +0530

    Time: 0 ms (100%), Space: 10.8 MB (53.77%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+// int fn(int i,vector<int>&nums,vector<int>&dp){
+//     if(i==0)
+//         return nums[i];
+//     if(i<0)
+//         return 0;
+//     if(dp[i]!=-1)
+//         return dp[i];
+//     int take=nums[i]+fn(i-2,nums,dp);
+//     int nottake=0+fn(i-1,nums,dp);
+//     return dp[i]=max(take,nottake);
+// }
     int fn(int i,vector<int>& nums,vector<int>&a,vector<int>&dp){
         if(i==0){
             if(a[0]==0)
@@ -12,11 +23,7 @@ public:
             return dp[i];
         if(i==nums.size()-1)
             a[0]=1;
-        int take=nums[i];
-        if(i>1)
-            take+=fn(i-2,nums,a,dp);
-        else
-            take+=0;
+        int take=nums[i]+fn(i-2,nums,a,dp);
         if(i==nums.size()-1)
             a[0]=0;
         int nottake=0+fn(i-1,nums,a,dp);
@@ -50,6 +57,12 @@ public:
         vector<int>a(1,0);
         vector<int>dp(nums.size(),-1);
         int ans=fn(n-1,nums,a,dp);
+        // vector<int>neglast(nums.begin(),nums.end()-1);
+        // vector<int>negfirst(nums.begin()+1,nums.end());
+        // vector<int>dp1(neglast.size(),-1);
+        // vector<int>dp2(negfirst.size(),-1);
+        // int ans=fn(neglast.size()-1,neglast,dp1);
+        // int res=fn(negfirst.size()-1,negfirst,dp2);
         vector<int>dpp(nums.size(),-1);
         int prev=0;
         int res=fnn(0,nums,prev,dpp);
