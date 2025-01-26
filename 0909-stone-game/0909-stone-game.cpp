f35755cb9798b1e481016969b2831e472dd2dd96
karmugilan28 <btechaids23226@smvec.ac.in>
2025-01-26 19:57:42 +0530

    Time: 0 ms (100%), Space: 10.4 MB (88.49%) - LeetHub

---
+++ 0909-stone-game/0909-stone-game.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // int sum=0,add=0;
+        // for(int i=0;i<piles.size();i++){
+        //     if(i%2==0){
+        //         sum=sum+piles[i];
+        //     }
+        //     else{
+        //         add=add+piles[i];
+        //     }
+        // }
+        // int def=0,ans=0;
+        // int flag=0,found=0;
+        // if(sum>add){
+        //     flag=1;
+        //     return true;
+        // }
+        
+        // else{
+        //     for(int i=0;i<piles.size();i++){
+        //         if(i<piles.size()/2){
+        //             def +=piles[i];
+        //         }
+        //         else
+        //             ans += piles[i];
+        //     }
+        // }
+        // if(def>ans){
+        //     found=1;
+        //     return true;
+        // }
+        // else
+        //     return false;
+        return true;
+    }
+};
