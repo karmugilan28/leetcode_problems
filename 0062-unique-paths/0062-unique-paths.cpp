e61903d09744c7965d144dff2a39333ea4b413af
karmugilan28 <btechaids23226@smvec.ac.in>
2025-01-29 15:25:02 +0530

    Time: 0 ms (100%), Space: 8.1 MB (83.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -19,22 +19,33 @@ public:
         // int ans=fn(m-1,n-1,dp);
         // return ans;
 
-        //tabulation
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0)
-                    dp[i][j]=1;
-                else{
-                    int l=0,r=0;
-                    if(i>0)
-                        l=dp[i-1][j];
-                    if(j>0)
-                        r=dp[i][j-1];
-                    dp[i][j]=l+r;
-                }
+        //tabulation with space of O(m*n);
+        // vector<vector<int>>dp(m,vector<int>(n,0));
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==0 && j==0)
+        //             dp[i][j]=1;
+        //         else{
+        //             int l=0,r=0;
+        //             if(i>0)
+        //                 l=dp[i-1][j];
+        //             if(j>0)
+        //                 r=dp[i][j-1];
+        //             dp[i][j]=l+r;
+        //         }
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+        //space optimization with O(n);
+        vector<int>dp(n,1);
+        for(int i=1;i<m;i++){
+            int prev=1;
+            for(int j=1;j<n;j++){
+                dp[j]=dp[j]+prev;
+                prev=dp[j];
             }
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
