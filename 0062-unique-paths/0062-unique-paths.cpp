d6d11eec658463cf59ed5444aef10d821a02aca8
karmugilan28 <btechaids23226@smvec.ac.in>
2025-01-29 15:22:34 +0530

    Time: 0 ms (100%), Space: 9.4 MB (33.6%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,40 @@
 class Solution {
-public:
-    int fn(int i,int j,vector<vector<int>>&dp){
-        if(i<0 || j<0)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(i==0 && j==0)
-            return 1;
-        int l=fn(i-1,j,dp);
-        int r=fn(i,j-1,dp);
-        return dp[i][j]=l+r;
-    }
+    //memoization takes O(m-1+n-1)(which the path we take) and space of  of O(n*m) + O(n-+m-1)
+// public:
+//     int fn(int i,int j,vector<vector<int>>&dp){
+//         if(i<0 || j<0)
+//             return 0;
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
+//         if(i==0 && j==0)
+//             return 1;
+//         int l=fn(i-1,j,dp);
+//         int r=fn(i,j-1,dp);
+//         return dp[i][j]=l+r;
+//     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        int ans=fn(m-1,n-1,dp);
-        return ans;
+        //memeoization
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // int ans=fn(m-1,n-1,dp);
+        // return ans;
+
+        //tabulation
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)
+                    dp[i][j]=1;
+                else{
+                    int l=0,r=0;
+                    if(i>0)
+                        l=dp[i-1][j];
+                    if(j>0)
+                        r=dp[i][j-1];
+                    dp[i][j]=l+r;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
