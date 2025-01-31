09b7fa88a00efc43c6165c662cd0a341c46b726e
karmugilan28 <btechaids23226@smvec.ac.in>
2025-01-31 19:29:04 +0530

    Time: 0 ms (100%), Space: 15.5 MB (78.33%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -23,22 +23,44 @@ public:
         // return ans;
 
         //tabulation
+        // int m=grid.size(),n=grid[0].size();
+        // vector<vector<int>>dp(m,vector<int>(n,0));         
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==0 && j==0)
+        //             dp[i][j]=grid[i][j];
+        //         else{
+        //             int l=INT_MAX,r=INT_MAX;
+        //             if(i>0)
+        //                 l=grid[i][j]+dp[i-1][j];
+        //             if(j>0)
+        //                 r=grid[i][j]+dp[i][j-1];
+        //             dp[i][j]=min(l,r);
+        //         }
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+        //space optimization
+        //I want space optimize from space of O(m*n) to O(n);
         int m=grid.size(),n=grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,0));         
-        for(int i=0;i<m;i++){
+        vector<int>dp(n,0);
+        int sum=0;
+        for(int j=0;j<n;j++){
+            sum+=grid[0][j];
+            dp[j]=sum;
+        }
+        for(int i=1;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0 && j==0)
-                    dp[i][j]=grid[i][j];
+                if(j==0)
+                    dp[j]=grid[i][j]+dp[j];
                 else{
-                    int l=INT_MAX,r=INT_MAX;
-                    if(i>0)
-                        l=grid[i][j]+dp[i-1][j];
-                    if(j>0)
-                        r=grid[i][j]+dp[i][j-1];
-                    dp[i][j]=min(l,r);
+                    int l=grid[i][j]+dp[j-1];
+                    int r=grid[i][j]+dp[j];
+                    dp[j]=min(l,r);
                 }
             }
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
