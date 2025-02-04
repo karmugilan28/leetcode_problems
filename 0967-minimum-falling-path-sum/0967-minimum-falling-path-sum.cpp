1ec3bcd873e11c897ca6f60877257c116104f700
karmugilan28 <btechaids23226@smvec.ac.in>
2025-02-04 19:41:48 +0530

    Time: 0 ms (100%), Space: 14.9 MB (75.7%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,33 +1,68 @@
 class Solution {
-public:
-    int fn(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp){  
-        if(j<0 || j>grid.size()-1)
-            return 99999;
-        if(dp[i][j]!=-1000)
-            return dp[i][j];
-        if(i==0 && j>=0)
-            return grid[i][j];
-        int l=INT_MAX,r=INT_MAX,g=INT_MAX,maxi=999999;
-        if(i==grid.size()-1){
-            for(int k=grid.size()-1;k>=0;k--){
-                l=grid[i][k]+fn(i-1,k,grid,dp);
-                r=grid[i][k]+fn(i-1,k-1,grid,dp);
-                g=grid[i][k]+fn(i-1,k+1,grid,dp);
-                maxi=min(maxi,min(l,min(r,g)));
-            }
-        }
-        else{
-            l=grid[i][j]+fn(i-1,j,grid,dp);
-            r=grid[i][j]+fn(i-1,j-1,grid,dp);
-            g=grid[i][j]+fn(i-1,j+1,grid,dp);
-        }
-        maxi=min(maxi,min(l,min(r,g)));
-        return dp[i][j]=maxi;
-    }
+//memoization
+// public:
+//     int fn(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp){  
+//         if(j<0 || j>grid.size()-1)
+//             return 99999;
+//         if(dp[i][j]!=-1000)
+//             return dp[i][j];
+//         if(i==0 && j>=0)
+//             return grid[i][j];
+//         int l=INT_MAX,r=INT_MAX,g=INT_MAX,maxi=999999;
+//         if(i==grid.size()-1){
+//             for(int k=grid.size()-1;k>=0;k--){
+//                 l=grid[i][k]+fn(i-1,k,grid,dp);
+//                 r=grid[i][k]+fn(i-1,k-1,grid,dp);
+//                 g=grid[i][k]+fn(i-1,k+1,grid,dp);
+//                 maxi=min(maxi,min(l,min(r,g)));
+//             }
+//         }
+//         else{
+//             l=grid[i][j]+fn(i-1,j,grid,dp);
+//             r=grid[i][j]+fn(i-1,j-1,grid,dp);
+//             g=grid[i][j]+fn(i-1,j+1,grid,dp);
+//         }
+//         maxi=min(maxi,min(l,min(r,g)));
+//         return dp[i][j]=maxi;
+//     }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
+        //memoization
+        // int n=matrix.size();
+        // vector<vector<int>>dp(n,vector<int>(n,-1000));
+        // return fn(n-1,n-1,matrix,dp);
+
+        //tabulation
         int n=matrix.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1000));
-        return fn(n-1,n-1,matrix,dp);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0)
+                    dp[i][j]=matrix[i][j];
+                else{
+                    int l=INT_MAX,r=INT_MAX,g=INT_MAX;
+                    if(j==0){
+                        l=matrix[i][j]+dp[i-1][j];
+                        r=matrix[i][j]+dp[i-1][j+1];
+                    }
+                    else if(j==n-1){
+                        g=matrix[i][j]+dp[i-1][j-1];
+                        l=matrix[i][j]+dp[i-1][j];
+                    }
+                    else{
+                        l=matrix[i][j]+dp[i-1][j];
+                        r=matrix[i][j]+dp[i-1][j+1];
+                        g=matrix[i][j]+dp[i-1][j-1];
+                    }
+                    dp[i][j]=min(l,min(r,g));
+                }
+            }
+        }
+        int mini=INT_MAX;
+        for(int j=0;j<n;j++){
+            if(dp[n-1][j]<mini)
+                mini=dp[n-1][j];
+        }
+        return mini;
     }
 };
