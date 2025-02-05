394e73496584023de954c7d7bdf52c5fca72f654
karmugilan28 <btechaids23226@smvec.ac.in>
2025-02-05 18:57:17 +0530

    Time: 0 ms (100%), Space: 15 MB (39.3%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -33,36 +33,68 @@ public:
         // return fn(n-1,n-1,matrix,dp);
 
         //tabulation
+        // int n=matrix.size();
+        // vector<vector<int>>dp(n,vector<int>(n,0));
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==0)
+        //             dp[i][j]=matrix[i][j];
+        //         else{
+        //             int l=INT_MAX,r=INT_MAX,g=INT_MAX;
+        //             if(j==0){
+        //                 l=matrix[i][j]+dp[i-1][j];
+        //                 r=matrix[i][j]+dp[i-1][j+1];
+        //             }
+        //             else if(j==n-1){
+        //                 g=matrix[i][j]+dp[i-1][j-1];
+        //                 l=matrix[i][j]+dp[i-1][j];
+        //             }
+        //             else{
+        //                 l=matrix[i][j]+dp[i-1][j];
+        //                 r=matrix[i][j]+dp[i-1][j+1];
+        //                 g=matrix[i][j]+dp[i-1][j-1];
+        //             }
+        //             dp[i][j]=min(l,min(r,g));
+        //         }
+        //     }
+        // }
+        // int mini=INT_MAX;
+        // for(int j=0;j<n;j++){
+        //     if(dp[n-1][j]<mini)
+        //         mini=dp[n-1][j];
+        // }
+        // return mini;
+
+        //space optimization
         int n=matrix.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
+        vector<int>dp(n);
+
         for(int i=0;i<n;i++){
+            vector<int>nums(n);
             for(int j=0;j<n;j++){
+                int l=INT_MAX,r=INT_MAX,g=INT_MAX;
                 if(i==0)
-                    dp[i][j]=matrix[i][j];
+                    nums[j]=matrix[i][j];
                 else{
-                    int l=INT_MAX,r=INT_MAX,g=INT_MAX;
                     if(j==0){
-                        l=matrix[i][j]+dp[i-1][j];
-                        r=matrix[i][j]+dp[i-1][j+1];
+                        l=matrix[i][j]+dp[j];
+                        r=matrix[i][j]+dp[j+1];
                     }
                     else if(j==n-1){
-                        g=matrix[i][j]+dp[i-1][j-1];
-                        l=matrix[i][j]+dp[i-1][j];
+                        g=matrix[i][j]+dp[j-1];
+                        l=matrix[i][j]+dp[j];
                     }
                     else{
-                        l=matrix[i][j]+dp[i-1][j];
-                        r=matrix[i][j]+dp[i-1][j+1];
-                        g=matrix[i][j]+dp[i-1][j-1];
+                        l=matrix[i][j]+dp[j];
+                        r=matrix[i][j]+dp[j+1];
+                        g=matrix[i][j]+dp[j-1];
                     }
-                    dp[i][j]=min(l,min(r,g));
+                    nums[j]=min(l,min(r,g));
                 }
             }
+            dp=nums;
         }
-        int mini=INT_MAX;
-        for(int j=0;j<n;j++){
-            if(dp[n-1][j]<mini)
-                mini=dp[n-1][j];
-        }
-        return mini;
+        int c=*min_element(dp.begin(),dp.end());
+        return c;
     }
 };
