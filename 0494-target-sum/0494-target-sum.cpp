0e3f48982e09a5a014d3da23d669b657378c714f
karmugilan28 <btechaids23226@smvec.ac.in>
2025-02-18 15:38:04 +0530

    Time: 930 ms (9.8%), Space: 11.7 MB (63.71%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -14,17 +14,21 @@ class Solution {
 //     }
 
 //dp
+//memoization
 public:
-    int fn(int i,vector<int>&nums,int tar,int sum){
+    int fn(int i,vector<int>&nums,int tar){
         if(i==nums.size()){
-            if(sum==tar)
+            if(tar==0)
                 return 1;
             return 0;
         }
-        return fn(i+1,nums,tar,sum+(nums[i]*(-1)))+fn(i+1,nums,tar,sum+nums[i]);
+        int l=fn(i+1,nums,tar-nums[i]*(-1));
+        int r=fn(i+1,nums,tar-nums[i]);
+        return l+r;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return fn(0,nums,target,0);
+        vector<int>dp(nums.size(),-10000);
+        return fn(0,nums,target);
     }
 };
