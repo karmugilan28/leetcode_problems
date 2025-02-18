092e1edd91da8bfb336e577d70ca6c7b4b97424b
karmugilan28 <btechaids23226@smvec.ac.in>
2025-02-18 12:26:40 +0530

    Time: 956 ms (5.76%), Space: 11.5 MB (87.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,30 @@
 class Solution {
+//backtracking 
+// public:
+//     void fn(int i,vector<int>&nums,int tar,int sum,int &cnt){
+//         if(i>=nums.size()){
+//             if(sum==tar){
+//                 cnt++;
+//             }
+//             return ;
+//         }
+//         fn(i+1,nums,tar,sum+(nums[i]*1),cnt);
+//         fn(i+1,nums,tar,sum+(nums[i]*(-1)),cnt);
+//         return ;
+//     }
+
+//dp
 public:
-    void fn(int i,vector<int>&nums,int tar,int sum,int &cnt){
-        if(i>=nums.size()){
-            if(sum==tar){
-                cnt++;
-            }
-            return ;
+    int fn(int i,vector<int>&nums,int tar,int sum){
+        if(i==nums.size()){
+            if(sum==tar)
+                return 1;
+            return 0;
         }
-        fn(i+1,nums,tar,sum+(nums[i]*1),cnt);
-        fn(i+1,nums,tar,sum+(nums[i]*(-1)),cnt);
-        return ;
+        return fn(i+1,nums,tar,sum+(nums[i]*(-1)))+fn(i+1,nums,tar,sum+nums[i]);
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int cnt=0;
-        fn(0,nums,target,0,cnt);
-        return cnt;
+        return fn(0,nums,target,0);
     }
 };
