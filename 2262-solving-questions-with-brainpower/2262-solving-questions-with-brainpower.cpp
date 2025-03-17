3826880035f646864a5c042b7481e3ee56a487bd
karmugilan28 <btechaids23226@smvec.ac.in>
2025-03-17 16:24:06 +0530

    Time: 79 ms (5.04%), Space: 146 MB (6.69%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,20 +1,34 @@
 class Solution {
-public:
-    long long fn(int i,vector<vector<int>>&arr,vector<long long>&dp){
-        if(i>=arr.size())
-            return 0;
+//memoization
+// public:
+//     long long fn(int i,vector<vector<int>>&arr,vector<long long>&dp){
+//         if(i>=arr.size())
+//             return 0;
 
-        if(dp[i] != -1)
-            return dp[i];
-            
-        long long take = arr[i][0] + fn(i+arr[i][1]+1,arr,dp);
-        long long nottake = 0 + fn(i+1,arr,dp);
-        return dp[i] = max(take,nottake);
-    }
+//         if(dp[i] != -1)
+//             return dp[i];
+
+//         long long take = arr[i][0] + fn(i+arr[i][1]+1,arr,dp);
+//         long long nottake = 0 + fn(i+1,arr,dp);
+//         return dp[i] = max(take,nottake);
+//     }
 public:
     long long mostPoints(vector<vector<int>>& questions) {
         int n=questions.size();
-        vector<long long> dp(n,-1);
-        return fn(0,questions,dp);
+        vector<vector<int>>arr(n,vector<int>(2,0));
+        arr=questions;
+        // vector<long long> dp(n,-1);
+        // return fn(0,questions,dp);
+        vector<long long>dp(n,0);
+        for(int i=n-1;i>=0;i--){
+            long long take = arr[i][0];
+            if((i+arr[i][1]+1) < n)
+                take += dp[i+arr[i][1]+1];
+            long long nottake = 0;
+            if(i<n-1)
+                nottake = dp[i+1];
+            dp[i] = max(take,nottake);
+        }
+        return dp[0];
     }
 };
