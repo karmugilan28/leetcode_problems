269b99432eb56fb5ce9b62ced8e62af38056af75
karmugilan28 <btechaids23226@smvec.ac.in>
2025-04-12 17:49:05 +0530

    Time: 2209 ms (5.01%), Space: 46.1 MB (14.07%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -11,24 +11,44 @@ class Solution {
 //         }
 //         return sum;      
 //     }
-int fn(int i,int k,vector<int>&coins,vector<vector<int>>&dp){
-    if(k==0)    return 1;
-    if(k<0)
-        return 0;
-    if(dp[i][k]!=-1)
-        return dp[i][k];
-    int sum=0;
-    for(int j=i;j>=0;j--){
-        int l=fn(j,k-coins[j],coins,dp);
-        sum+=l;
-    }
-    return dp[i][k]=sum;
-}
+//memoization
+// int fn(int i,int k,vector<int>&coins,vector<vector<int>>&dp){
+//     if(k==0)    return 1;
+//     if(k<0)
+//         return 0;
+//     if(dp[i][k]!=-1)
+//         return dp[i][k];
+//     int sum=0;
+//     for(int j=i;j>=0;j--){
+//         int l=fn(j,k-coins[j],coins,dp);
+//         sum+=l;
+//     }
+//     return dp[i][k]=sum;
+// }
 public:
     int change(int amount, vector<int>& coins) {
         int k=amount;
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(k+1,-1));
-        return fn(n-1,k,coins,dp);
+        // vector<vector<int>>dp(n,vector<int>(k+1,-1));
+        // return fn(n-1,k,coins,dp);
+        //tabulation
+        vector<vector<long long>>dp(n,vector<long long>(k+1,0));
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+        long long mod=1e11+7;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=k;j++){
+                long long sum=0;
+                for(int g=i;g>=0;g--){
+                    long long l=0;
+                    if(j-coins[g]>=0)
+                        l=dp[g][j-coins[g]];
+                    sum+=(l%mod);
+                }
+                dp[i][j]=sum;
+            }
+        }
+        return dp[n-1][k];
     }
 };
