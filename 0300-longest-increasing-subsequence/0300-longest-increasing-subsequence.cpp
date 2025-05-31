7d121d81db7c161fed3d9155481b327f50dc1ddf
karmugilan28 <btechaids23226@smvec.ac.in>
2025-05-31 10:53:10 +0530

    Time: 434 ms (24.77%), Space: 331.6 MB (5.93%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int fn(int i,int last,vector<int>&nums,vector<vector<int>>&dp){
-        if(i<0)
-            return 0;
+        if(i==0){
+            return nums[0]<last;
+        }
         if(dp[i][last]!=-1) return dp[i][last];
         int take=0;
         if(nums[i]<last)
@@ -24,9 +25,26 @@ public:
                 nums[i]+=abs(mini);
             }
         }
+        //memoization
+        // int maxi=*max_element(nums.begin(),nums.end());
+        // vector<vector<int>>dp(n,vector<int>(maxi+2,-1))
+        // return fn(n-1,maxi+1,nums,dp);
+
+        //tabulation
         int maxi=*max_element(nums.begin(),nums.end());
-        vector<vector<int>>dp(n,vector<int>(maxi+2,-1));
-        return fn(n-1,maxi+1,nums,dp);
+        vector<vector<int>>dp(n,vector<int>(maxi+2,0));
+        for(int j=0;j<=maxi+1;j++)  dp[0][j]=nums[0]<j;
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=maxi+1;j++){
+                int take=0;
+                if(nums[i]<j)
+                    take+=(1+dp[i-1][nums[i]]);
+                int nottake=0+dp[i-1][j];
+                dp[i][j]=max(take,nottake);
+            }
+        }
+        return dp[n-1][maxi+1];
         
     }
 };
