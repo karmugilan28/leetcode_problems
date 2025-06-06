9c251c6d4a7bd7c7cc8df678f0857a94084d8ef3
karmugilan28 <btechaids23226@smvec.ac.in>
2025-06-06 12:06:15 +0530

    Time: 47 ms (90.66%), Space: 76 MB (21.4%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,21 +1,18 @@
 class Solution {
-public:
-    int fn(int i,int j,string& s,vector<vector<int>>&dp){
-        if(i>j) return 0;
-        if(i==j)    return 1;
-        if(dp[i][j]!=-1)    return dp[i][j];
-        int pick=0,notpick1=0,notpick2=0;
-        if(s[i]==s[j])  pick=2+fn(i+1,j-1,s,dp);
-        else{
-            notpick1=fn(i+1,j,s,dp);
-            notpick2=fn(i,j-1,s,dp);
-        }
-        return dp[i][j] = max(pick,max(notpick1,notpick2));
-    }
+int fn(int l,int r,string& s,vector<vector<int>>&dp){
+    if(l>r) return 0;
+    if(l==r)    return 1;
+    if(dp[l][r]!=-1)    return dp[l][r];
+    if(s[l]==s[r])  return dp[l][r] = 2 + fn(l+1,r-1,s,dp);
+    else    return dp[l][r] = max(fn(l+1,r,s,dp),fn(l,r-1,s,dp));
+}
 public:
     int longestPalindromeSubseq(string s) {
         int n=s.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
         return fn(0,n-1,s,dp);
+
+        //tabulation
+        
     }
 };
