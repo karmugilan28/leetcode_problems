24680abb4a922914496a18eae4cfbda25ac7c141
karmugilan28 <btechaids23226@smvec.ac.in>
2025-06-07 11:24:36 +0530

    Time: 18 ms (90.97%), Space: 9.6 MB (98.24%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -19,13 +19,24 @@ public:
         // return n - f;
 
         //tabulation
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= n; j++){
+        //         if(s[i-1] == t[j-1])    dp[i][j] = 1 + dp[i-1][j-1];
+        //         else    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+        //     }
+        // }
+        // return  n - dp[n][n];
+
+        //space optimization
+        vector<int>prev(n+1,0),curr(n+1,0);
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
-                if(s[i-1] == t[j-1])    dp[i][j] = 1 + dp[i-1][j-1];
-                else    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                if(s[i-1] == t[j-1])    curr[j] = 1 + prev[j-1];
+                else    curr[j] = max(prev[j] , curr[j-1]);
             }
+            prev = curr;
         }
-        return  n - dp[n][n];
+        return  n - prev[n];
     }
 };
