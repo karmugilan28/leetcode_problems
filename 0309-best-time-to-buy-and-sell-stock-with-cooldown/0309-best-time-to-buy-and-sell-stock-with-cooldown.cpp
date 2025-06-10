022874991fbd37a4f0a7552645351e863f5a131e
karmugilan28 <btechaids23226@smvec.ac.in>
2025-06-10 12:14:12 +0530

    Time: 3 ms (35.13%), Space: 16.2 MB (34.58%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,21 +1,41 @@
 class Solution {
+//memoization
+// public:
+    // int fn(int i,int last,vector<int>&nums,vector<vector<int>>&dp){
+    //     if(i<0) return 0;
+    //     if(i == 0){
+    //         if(last==1)    return dp[i][last] = -nums[i];
+    //         else    return dp[i][last] = 0;
+    //     }
+    //     if(dp[i][last] != -1)  return dp[i][last];
+    //     int nottake = fn(i-1,last,nums,dp),take = 0;
+    //     if(last == 0)   take = nums[i] + fn(i-1,1,nums,dp);
+    //     else    take = -nums[i] + fn(i-2,0,nums,dp);
+    //     return dp[i][last] = max(take,nottake);
+    // }
 public:
-    int fn(int i,int last,vector<int>&nums,vector<vector<int>>&dp){
-        if(i<0) return 0;
-        if(i == 0){
-            if(last==1)    return dp[i][last] = -nums[i];
-            else    return dp[i][last] = 0;
+    int maxProfit(vector<int>& nums) {
+        int n = nums.size();
+        //memoization
+        // vector<vector<int>>dp(n,vector<int>(2,-1));
+        // return fn(n-1,0,prices,dp);
+
+        //tabulation
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        dp[1][1] = -nums[0];
+        dp[1][0] = 0;
+        for(int i = 2; i <= n; i++){
+            for(int j = 0; j <= 1; j++){
+                int nottake = dp[i-1][j];
+                int take = 0;
+                if(j == 0)   take = nums[i-1] + dp[i-1][1];
+                else{
+                    if(i>1)  take = -nums[i-1] + dp[i-2][0];
+                }
+                dp[i][j] = max(nottake,take);
+            }
         }
-        if(dp[i][last] != -1)  return dp[i][last];
-        int nottake = fn(i-1,last,nums,dp),take = 0;
-        if(last == 0)   take = nums[i] + fn(i-1,1,nums,dp);
-        else    take = -nums[i] + fn(i-2,0,nums,dp);
-        return dp[i][last] = max(take,nottake);
-    }
-public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return fn(n-1,0,prices,dp);
+        // for(int i=0 ;i<=n;i++)  cout << dp[i][0] << ' ' << dp[i][1] << endl;
+        return dp[n][0];
     }
 };
