a86ac5610b5b02c824a0b9c979f2229c8834dd74
karmugilan28 <btechaids23226@smvec.ac.in>
2025-06-13 06:53:01 +0530

    Time: 31 ms (55.06%), Space: 44.2 MB (32.13%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -21,6 +21,8 @@ public:
         // vector<vector<int>>dp(n,vector<int>(m,-1));
         // int f = fn(n-1,m-1,s,t,dp);
 
+        //tabulation
+
         vector<vector<long long>>dp(n+1,vector<long long>(m+1,0));
         for(int i = 0; i <= n; i++)
             dp[i][0] = 1;
@@ -40,7 +42,20 @@ public:
         return (int)dp[n][m];
         
         //tabulation
-        // 
+    
+        vector<long long>prev(m+1,0);
+        prev[0] = 1;        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(s[i-1] == t[j-1]){
+                    long long take = prev[j-1];
+                    long long nottake = prev[j];
+                    if(take + nottake > INT_MAX)    prev[j] = INT_MAX;
+                    else    prev[j] = take + nottake;
+                }
+            }
+        }
+        return (int)prev[m];
         
     }
 };
