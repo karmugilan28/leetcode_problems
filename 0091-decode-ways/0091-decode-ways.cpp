d48e9e09ef517fc7c988486897b0ec0a15033e14
karmugilan28 <btechaids23226@smvec.ac.in>
2025-07-13 11:09:04 +0530

    Time: 0 ms (100%), Space: 9.8 MB (21.86%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,19 +1,20 @@
 class Solution {
-public:
-    int fn(int i,int flg,string& s,vector<vector<int>>&dp){
-        if(i == s.size())   return 1;
-        if(dp[i][flg] != -1)    return dp[i][flg];
-        int one = 0,two = 0;
-        if(s[i] != '0' )    one = fn(i+1,1,s,dp);
-        if(flg == 1){
-            string res = "";
-            res.push_back(s[i-1]);
-            res.push_back(s[i]);
-            int num = stoi(res);
-            if(num <= stoi("26"))   two = fn(i+1,0,s,dp);
-        }
-        return dp[i][flg] = one+two;
-    }
+//memoization
+// public:
+//     int fn(int i,int flg,string& s,vector<vector<int>>&dp){
+//         if(i < 0)   return 1;
+//         if(dp[i][flg] != -1)    return dp[i][flg];
+//         int one = 0,two = 0;
+//         if(s[i] != '0' )    one = fn(i-1,1,s,dp);
+//         if(flg == 1){
+//             string res = "";
+//             res.push_back(s[i]);
+//             res.push_back(s[i+1]);
+//             int num = stoi(res);
+//             if(num <= stoi("26"))   two = fn(i-1,0,s,dp);
+//         }
+//         return dp[i][flg] = one+two;
+//     }
 public:
     int numDecodings(string s) {
         int n = s.size();
@@ -25,7 +26,27 @@ public:
                 }
             }
         }
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return fn(0,0,s,dp);
+        // memoization
+        // vector<vector<int>>dp(n,vector<int>(2,-1));
+        // return fn(n-1,0,s,dp);
+
+        //tabulation
+        vector<vector<int>>dp(n+1,vector<int>(2,-0));
+        dp[0][0] = 1,dp[0][1] = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j < 2; j++){
+                int one = 0,two = 0;
+                if(s[i-1] != '0')   one = dp[i-1][1];
+                if(j == 1 && i > 1){
+                    string res = "";
+                    res.push_back(s[i-2]);
+                    res.push_back(s[i-1]);
+                    int num = stoi(res);
+                    if(num <= stoi("26"))   two = dp[i-1][0];
+                }
+                dp[i][j] = one+two;
+            }
+        }
+        return dp[n][1];
     }
 };
