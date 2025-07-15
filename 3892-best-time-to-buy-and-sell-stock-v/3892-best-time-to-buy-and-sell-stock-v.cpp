7c5efb05115446c45b1adb466daea192443858e1
karmugilan28 <btechaids23226@smvec.ac.in>
2025-07-15 19:28:58 +0530

    Time: 343 ms (59.26%), Space: 178.6 MB (46.2%) - LeetHub

---
+++ 3892-best-time-to-buy-and-sell-stock-v/3892-best-time-to-buy-and-sell-stock-v.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long fn(int i, int buy,int k,vector<int>&nums,vector<vector<vector<long long>>>&dp){
+        if(i == 0){
+            if(buy == 0)    return -nums[i];
+            if(buy == 2)    return nums[i];
+            return 0;
+        }
+        if(k == 0)  return 0;
+        if(dp[i][buy][k] != -1) return dp[i][buy][k];
+        //transaction 1
+        long long notdo = fn(i-1,buy,k,nums,dp),tar1 = -1e9,tar2 = -1e9,tar3 = -1e9, tar4 = -1e9;
+        if((buy == 1 || buy == 3)&& k > 0)   tar1 = max(notdo,nums[i] +fn(i-1,0,k,nums,dp));
+        else if((buy == 0) && k > 0)   tar2 = max(notdo,-nums[i]+fn(i-1,1,k-1,nums,dp));
+
+        //transaction 2
+        if((buy == 3 || buy == 1)&& k > 0)   tar3 = max(notdo,-nums[i]+fn(i-1,2,k,nums,dp));
+        else if((buy == 2)&& k > 0)  tar4 = max(notdo,nums[i]+fn(i-1,3,k-1,nums,dp));
+        return dp[i][buy][k] = max(tar1,max(tar2,max(tar3,tar4)));
+    }
+public:
+    long long maximumProfit(vector<int>& prices, int k) {
+        int n = prices.size();
+        vector<vector<vector<long long>>>dp(n,vector<vector<long long>>(4,vector<long long>(k+1,-1)));
+        return fn(n-1,1,k,prices,dp);
+    }
+};
