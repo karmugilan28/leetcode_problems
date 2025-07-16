b3a8963acbc6f197ca61356ce0d818fc152a0473
karmugilan28 <btechaids23226@smvec.ac.in>
2025-07-16 14:27:17 +0530

    Time: 7 ms (97.3%), Space: 9.6 MB (97.36%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,29 +1,84 @@
 class Solution {
+//memoization
+// public:
+//     int fn(int i,int j,string &s,string &t,vector<vector<int>>&dp){
+//         if(i < 0 ){
+//             int sum = 0;
+//             for(int k = 0; k <= j; k++) sum += int(t[k]);
+//             return sum; 
+//         }
+//         if(j < 0){
+//             int sum = 0;
+//             for(int k = 0; k <= i; k++) sum += int(s[k]);
+//             return sum; 
+//         }
+//         if(dp[i][j] != -1)  return dp[i][j];
+//         int take = 0;
+//         if(s[i] == t[j])    return dp[i][j] = fn(i-1,j-1,s,t,dp);
+//         else{
+//             int nottake1 = int(s[i]) + fn(i-1,j,s,t,dp);
+//             int nottake2 = int(t[j]) + fn(i,j-1,s,t,dp);
+//             return dp[i][j] = min(nottake1,nottake2);
+//         }   
+//     }
 public:
-    int fn(int i,int j,string &s,string &t,vector<vector<int>>&dp){
-        if(i < 0 ){
-            int sum = 0;
-            for(int k = 0; k <= j; k++) sum += int(t[k]);
-            return sum; 
+    int minimumDeleteSum(string s, string t) {
+        int n = s.size(), m = t.size();
+        //memoization
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // return fn(n-1,m-1,s,t,dp);
+
+        //tabulation
+        //base case
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        // int sum = 0;
+        // for(int j = 1; j <= m; j++){
+        //     sum += int(t[j-1]);
+        //     dp[0][j] = sum;
+        // }
+        // int sum2 = 0;
+        // for(int i = 1; i <= n; i++){
+        //     sum2 += int(s[i-1]);
+        //     dp[i][0] = sum2;
+        // }
+
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= m; j++){
+        //         int take = 0;
+        //         if(s[i-1] == t[j-1])    dp[i][j] = dp[i-1][j-1];
+        //         else{
+        //             int nottake1 = int(s[i-1]) + dp[i-1][j];
+        //             int nottake2 = int(t[j-1]) + dp[i][j-1];
+        //             dp[i][j] = min(nottake1,nottake2);
+        //         }
+        //     }
+        // }
+        // return dp[n][m];
+
+        // space optimization
+        vector<int>prev(m+1),curr(m+1);
+        int sum = 0;
+        for(int j = 1; j <= m; j++){
+            sum += int(t[j-1]);
+            prev[j] = sum;
         }
-        if(j < 0){
-            int sum = 0;
-            for(int k = 0; k <= i; k++) sum += int(s[k]);
-            return sum; 
+        int sum2 = 0;
+        
+
+        for(int i = 1; i <= n; i++){
+            sum2 += int(s[i-1]);
+            curr[0] = sum2;
+            for(int j = 1; j <= m; j++){
+                int take = 0;
+                if(s[i-1] == t[j-1])    curr[j] = prev[j-1];
+                else{
+                    int nottake1 = int(s[i-1]) + prev[j];
+                    int nottake2 = int(t[j-1]) + curr[j-1];
+                    curr[j] = min(nottake1,nottake2);
+                }
+            }
+            prev = curr;
         }
-        if(dp[i][j] != -1)  return dp[i][j];
-        int take = 0;
-        if(s[i] == t[j])    return dp[i][j] = fn(i-1,j-1,s,t,dp);
-        else{
-            int nottake1 = int(s[i]) + fn(i-1,j,s,t,dp);
-            int nottake2 = int(t[j]) + fn(i,j-1,s,t,dp);
-            return dp[i][j] = min(nottake1,nottake2);
-        }   
-    }
-public:
-    int minimumDeleteSum(string s1, string s2) {
-        int n = s1.size(), m = s2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return fn(n-1,m-1,s1,s2,dp);
+        return prev[m];
     }
 };
