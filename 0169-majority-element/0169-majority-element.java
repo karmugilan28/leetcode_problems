bee66417ec87e7b658a203ab05ce30a24710bf78
karmugilan28 <btechaids23226@smvec.ac.in>
2025-07-18 14:33:44 +0530

    Time: 2 ms (73.67%), Space: 53.2 MB (49.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,13 +1,23 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> mpp = new HashMap<Integer,Integer>();
-        for(int i = 0; i < nums.length ; i++){
-            mpp.put(nums[i],mpp.getOrDefault(nums[i],0)+1);
+        // HashMap<Integer,Integer> mpp = new HashMap<Integer,Integer>();
+        // for(int i = 0; i < nums.length ; i++){
+        //     mpp.put(nums[i],mpp.getOrDefault(nums[i],0)+1);
+        // }
+        // for(Map.Entry<Integer,Integer> entry: mpp.entrySet()){
+        //     if(entry.getValue() >= (nums.length+1)/2)
+        //         return entry.getKey();
+        // }
+        int n = nums.length;
+        int prev = 0,ct = 0;
+        for(int i = 0; i < n; i++){
+            if(ct == 0){
+                prev = nums[i];
+                ct++;
+            }
+            else if(prev == nums[i])    ct++;
+            else if(prev != nums[i])    ct--;
         }
-        for(Map.Entry<Integer,Integer> entry: mpp.entrySet()){
-            if(entry.getValue() >= (nums.length+1)/2)
-                return entry.getKey();
-        }
-        return 0;
+        return prev;
     }
 }
