bc37e7f1eeeb237fad56ea465be993cbfceb75a2
karmugilan28 <btechaids23226@smvec.ac.in>
2025-08-01 20:43:51 +0530

    Time: 57 ms (3.82%), Space: 13.7 MB (8.53%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -10,6 +10,7 @@ class Solution {
 //         int r=grid[i][j]+fn(i+1,j+1,grid,m,dp);
 //         return dp[i][j]=min(l,r);
 //     }
+
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         //memoization
@@ -26,25 +27,47 @@ public:
         // int ans=fn(0,0,triangle,m-1,dp);
         // return ans;
 
-        //tabulation
-        int m=triangle.size();
+        //tabulation with time complexity of 
+        // int m=triangle.size();
+        // vector<vector<int>>dp;
+        // for(int i=0;i<m;i++){
+        //     vector<int>nums;
+        //     for(int j=0;j<triangle[i].size();j++){
+        //         nums.push_back(0);
+        //     }
+        //     dp.push_back(nums);
+        // }
+        // for(int i=m-1;i>=0;i--){
+        //     for(int j=triangle[i].size()-1;j>=0;j--){
+        //         if(i==m-1)
+        //             dp[i][j]=triangle[i][j];
+        //         else{
+        //             int l=triangle[i][j]+dp[i+1][j];
+        //             int r=triangle[i][j]+dp[i+1][j+1];
+        //             dp[i][j]=min(l,r);
+        //         }
+        //     }
+        // }
+        // return dp[0][0];
+
+        //space optimization
+        int n  = triangle.size();
         vector<vector<int>>dp;
-        for(int i=0;i<m;i++){
-            vector<int>nums;
-            for(int j=0;j<triangle[i].size();j++){
-                nums.push_back(0);
-            }
-            dp.push_back(nums);
+        for(int i = 0; i < n; i++){
+            vector<int>a;
+            for(int j = 0; j < i+1; j++)    a.push_back(0);
+            dp.push_back(a);
         }
-        for(int i=m-1;i>=0;i--){
-            for(int j=triangle[i].size()-1;j>=0;j--){
-                if(i==m-1)
-                    dp[i][j]=triangle[i][j];
-                else{
-                    int l=triangle[i][j]+dp[i+1][j];
-                    int r=triangle[i][j]+dp[i+1][j+1];
-                    dp[i][j]=min(l,r);
-                }
+        for(int i = 0; i < n; i++)    dp[n-1][i] = triangle[n-1][i];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < dp[i].size(); j++)   cout << dp[i][j] << " ";
+            cout << endl;  
+        }
+        for(int i = n-2; i >= 0; i--){
+            for(int j = 0; j < triangle[i].size(); j++){
+                int d = triangle[i][j] + dp[i+1][j+1];
+                int e = triangle[i][j] + dp[i+1][j];
+                dp[i][j] = min(d,e);
             }
         }
         return dp[0][0];
