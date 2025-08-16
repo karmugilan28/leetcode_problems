f2762832ab9ca8d18c3ccdd293e3e7c1c3533c05
karmugilan28 <btechaids23226@smvec.ac.in>
2025-08-16 08:15:55 +0530

    Time: 0 ms (100%), Space: 8 MB (71.54%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string ans= to_string(num);
+        int n=ans.size();
+        for(int i=0;i<n;i++){
+            if(ans[i]=='6'){
+                ans[i]='9';
+                break;
+            }
+        }
+        int answer=stoi(ans);
+        return answer;
+    }
+};
