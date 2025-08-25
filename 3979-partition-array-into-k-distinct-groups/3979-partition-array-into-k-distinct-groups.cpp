dc2b09896becac564139142458e1c13ddfd52fad
karmugilan28 <btechaids23226@smvec.ac.in>
2025-08-25 20:38:58 +0530

    Time: 96 ms (12.5%), Space: 272.5 MB (29.12%) - LeetHub

---
+++ 3979-partition-array-into-k-distinct-groups/3979-partition-array-into-k-distinct-groups.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool partitionArray(vector<int>& arr, int k) {
+        int n = arr.size();
+        // map<int,int>mpp,mp1;
+        // vector<int>nums;
+        // for(int i = 0; i < n; i++)  mp1[arr[i]] += 1;
+        // int flg = 1;
+        // while(flg == 1){
+        //     flg = 0;
+        //     for(auto&it:mp1){
+        //         if(it.second > 0)   {nums.push_back(it.first);it.second -= 1;flg = 1;}
+        //         else    mp1.erase(it.first);
+        //     }
+        // }
+        // for(int i = 0; i < n; i++)  cout << nums[i] << " ";
+        // cout << endl;
+        // for(int i = 0; i < n; i++){
+        //     if(mpp.size() >= k) mpp.clear();
+        //     if(mpp.size() < k)    mpp[nums[i]] += 1;
+        //     if(mpp.size() >= k) mpp.clear();
+        //     // cout << mpp.size() << endl;
+        // }
+        // if(mpp.size() == 0) return true;
+        // else{
+
+        // }
+        // return false;
+        if(n % k == 0){
+            int a = n/k;
+            map<int,int>mpp;
+            for(int i = 0; i < n; i++)  mpp[arr[i]] += 1;
+            for(int i = 0 ; i < n; i++){
+                if(mpp[arr[i]] > a) return false;
+            }
+            return true;
+        }
+        return false;
+    }
+};
