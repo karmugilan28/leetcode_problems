ef11d824adb23bdaf9fd420374bb59dd8b41d0bd
karmugilan28 <btechaids23226@smvec.ac.in>
2026-03-16 16:26:25 +0530

    Time: 582 ms (13.33%), Space: 286.6 MB (46.67%) - LeetHub

---
+++ 3868-minimum-cost-to-equalize-arrays-using-swaps/3868-minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minCost(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        map<int,int>mpp;
+        for(int i = 0; i < n; i++)  mpp[nums1[i]]++;
+        for(int i = 0; i < n; i++)  mpp[nums2[i]]++;
+
+        for(auto&it:mpp){
+            if(it.second%2 != 0)    return -1;
+        }
+
+        map<int,int>diff;
+        for(int i = 0; i < n; i++)  diff[nums1[i]]++;
+        for(int i = 0; i < n; i++)  diff[nums2[i]]--;
+
+        long long sum = 0;
+        for(auto&it:diff){
+            sum += abs((long long)it.second);
+        }
+        return sum/4;
+    }
+};
