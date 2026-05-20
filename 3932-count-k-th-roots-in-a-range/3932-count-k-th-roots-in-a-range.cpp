ab6213ef21b192a2dd4511476c87d987d830a713
karmugilan28 <btechaids23226@smvec.ac.in>
2026-05-20 11:16:51 +0530

    Time: 0 ms (100%), Space: 8.7 MB (82.66%) - LeetHub

---
+++ 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool check(long long mid, int k, long long n) {
+        if(mid == 0) return true;
+        long long ans = 1;
+        for(int i = 0; i < k; i++) {
+            if(ans > n / mid) return false;
+            ans *= mid;
+        }
+        return ans <= n;
+    }
+    long long kthRoot(long long n, int k) {
+        long long low = 0;
+        long long high = n;
+        long long ans = 0;
+        while(low <= high) {
+            long long mid = low+(high-low)/2;
+            if(check(mid, k, n)){
+                ans = mid;
+                low = mid + 1;
+            }
+            else    high = mid - 1;
+        }
+        return ans;
+    }
+
+    int countKthRoots(int l, int r, int k) {
+        long long right = kthRoot(r, k);
+        long long left = kthRoot((long long)l - 1, k);
+        long long ans = right - left;
+
+        if(l == 0) ans++;
+        return (int)ans;
+
+    }
+};
