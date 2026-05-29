95c4c334cbd1b23e3e95cb4ec18000d91b382508
karmugilan28 <btechaids23226@smvec.ac.in>
2026-05-29 19:08:18 +0530

    Time: 0 ms (100%), Space: 20.6 MB (29.5%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,46 +1,74 @@
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& adj_mat) {
-        int n = adj_mat.size();
-        vector<vector<int>> adj(n + 1);
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(i != j && adj_mat[i][j] != 0){
-                    adj[i+1].push_back(j+1);
-                }
-            }
-        }
-        int cnt = 0;
-        // for(int i = 1; i <= n; i++){
-        //     if(adj[i].size() == 0){
-        //         cnt++;
+        // int n = adj_mat.size();
+        // vector<vector<int>> adj(n + 1);
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < n; j++){
+        //         if(i != j && adj_mat[i][j] != 0){
+        //             adj[i+1].push_back(j+1);
+        //         }
         //     }
         // }
+        // int cnt = 0;
+        // // for(int i = 1; i <= n; i++){
+        // //     if(adj[i].size() == 0){
+        // //         cnt++;
+        // //     }
+        // // }
+        // queue<int>q;
+        // vector<int>vis(n+1,0);
+        // int flg = 0,rem;
+        // int ct = 0;
+        // for(int i = 1; i <= n; i++){
+        //     if(vis[i] == 0) {rem = i;flg = 1;break;}
+        // }
+        // while(flg){
+        //     q.push(rem);
+        //     vis[rem] = 1;
+        //     while(!q.empty()){
+        //         int a = q.front();
+        //         for(int i = 0; i < adj[a].size(); i++){
+        //             if(!vis[adj[a][i]]){
+        //                 q.push(adj[a][i]);
+        //                 vis[adj[a][i]] = 1;
+        //             }
+        //         }
+        //         q.pop();
+        //     }
+        //     flg = 0;
+        //     for(int i = 1; i <= n; i++){
+        //         if(vis[i] == 0) {rem = i;flg = 1;break;}
+        //     }
+        //     ct++;
+        // }
+        // return ct;
+        int n = adj_mat.size();
+        vector<vector<int>>adj(n+1,vector<int>());
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i != j && adj_mat[i][j])    adj[i+1].push_back(j+1);
+            }
+        }
         queue<int>q;
         vector<int>vis(n+1,0);
-        int flg = 0,rem;
         int ct = 0;
         for(int i = 1; i <= n; i++){
-            if(vis[i] == 0) {rem = i;flg = 1;break;}
-        }
-        while(flg){
-            q.push(rem);
-            vis[rem] = 1;
-            while(!q.empty()){
-                int a = q.front();
-                for(int i = 0; i < adj[a].size(); i++){
-                    if(!vis[adj[a][i]]){
-                        q.push(adj[a][i]);
-                        vis[adj[a][i]] = 1;
+            if(!vis[i]){
+                vis[i] = 1;
+                q.push(i);
+                while(!q.empty()){
+                    int node = q.front();
+                    for(auto it: adj[node]){
+                        if(!vis[it]){
+                            vis[it] = 1;
+                            q.push(it);
+                        }
                     }
+                    q.pop();
                 }
-                q.pop();
+                ct++;
             }
-            flg = 0;
-            for(int i = 1; i <= n; i++){
-                if(vis[i] == 0) {rem = i;flg = 1;break;}
-            }
-            ct++;
         }
         return ct;
     }
